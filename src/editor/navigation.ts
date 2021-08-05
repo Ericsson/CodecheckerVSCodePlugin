94e646d4f524e2e080dc5937a1f4681b25471224
Discookie <matekos17@fazekas.hu>
2021-08-06 01:37:26 +0200

    Add keybindings for repr-step navigation

--- src/editor/navigation.ts
+++ src/editor/navigation.ts
@@ -1,4 +1,4 @@
-import { ExtensionContext, Range, Uri, commands, window } from 'vscode';
+import { ExtensionContext, Position, Range, Uri, commands, window } from 'vscode';
 import { ExtensionApi } from '../backend/api';
 import { AnalysisPathEvent, AnalysisPathKind, DiagnosticEntry } from '../backend/types';
 
@@ -7,6 +7,8 @@ export class NavigationHandler {
         ctx.subscriptions.push(commands.registerCommand('codechecker.editor.toggleSteps', this.toggleSteps, this));
         ctx.subscriptions.push(commands.registerCommand('codechecker.editor.jumpToReport', this.jumpToReport, this));
         ctx.subscriptions.push(commands.registerCommand('codechecker.editor.jumpToStep', this.jumpToStep, this));
+        ctx.subscriptions.push(commands.registerCommand('codechecker.editor.nextStep', this.nextStep, this));
+        ctx.subscriptions.push(commands.registerCommand('codechecker.editor.previousStep', this.previousStep, this));
     }
 
     toggleSteps(file: Uri | string, diagnosticIdx: number, targetState?: boolean) {
@@ -78,6 +80,111 @@ export class NavigationHandler {
             window.showInformationMessage('Unable to find specified report, opened its file instead');
         } else if (step === undefined) {
             window.showInformationMessage('Unable to find specified reproduction step, opened the report instead');
+        }
+    }
+
+    getStepIndexUnderCursor(which: 'first' | 'last'): number | null {
+        if (window.activeTextEditor === undefined || ExtensionApi.diagnostics.selectedEntry === undefined) {
+            return null;
+        }
+
+        const cursor = window.activeTextEditor.selection.anchor;
+
+        const entry = ExtensionApi.diagnostics.selectedEntry.diagnostic;
+        const reprPath = entry.path
+            .filter(e => e.kind === AnalysisPathKind.Event) as AnalysisPathEvent[];
+
+        let foundIdx = null;
+
+        for (const [idx, path] of reprPath.entries()) {
+            // Check location first
+            if (window.activeTextEditor.document.uri.fsPath !== entry.files[path.location.file]) {
+                continue;
+            }
+
+            if (cursor.isEqual(new Position(path.location.line-1, path.location.col-1))) {
+                foundIdx = idx;
+
+                if (which === 'first') {
+                    break;
+                }
+
+                continue;
+            }
+
+            // Check inside the ranges
+            if (!path.ranges) {
+                continue;
+            }
+
+            for (const [start, end] of path.ranges) {
+                const range = new Range(
+                    start.line-1,
+                    start.col-1,
+                    end.line-1,
+                    end.col,
+                );
+
+                if (range.contains(cursor)) {
+                    foundIdx = idx;
+
+                    if (which === 'first') {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return foundIdx;
+    }
+
+    nextStep() {
+        const stepIdx = this.getStepIndexUnderCursor('last');
+        const entry = ExtensionApi.diagnostics.selectedEntry?.diagnostic;
+
+        if (stepIdx === null || !entry) {
+            return;
+        }
+
+        const reprPath = entry.path
+            .filter(e => e.kind === AnalysisPathKind.Event) as AnalysisPathEvent[];
+
+        if (stepIdx < reprPath.length - 1) {
+            const stepLocation = reprPath[stepIdx + 1].location;
+
+            window.showTextDocument(Uri.file(entry.files[stepLocation.file]), {
+                selection: new Range(
+                    stepLocation.line - 1,
+                    stepLocation.col - 1,
+                    stepLocation.line - 1,
+                    stepLocation.col - 1
+                )
+            });
+        }
+    }
+
+    previousStep() {
+        const stepIdx = this.getStepIndexUnderCursor('first');
+        const entry = ExtensionApi.diagnostics.selectedEntry?.diagnostic;
+
+        if (stepIdx === null || !entry) {
+            return;
+        }
+
+        const reprPath = entry.path
+            .filter(e => e.kind === AnalysisPathKind.Event) as AnalysisPathEvent[];
+
+        if (stepIdx > 0) {
+            const stepLocation = reprPath[stepIdx - 1].location;
+
+            window.showTextDocument(Uri.file(entry.files[stepLocation.file]), {
+                selection: new Range(
+                    stepLocation.line - 1,
+                    stepLocation.col - 1,
+                    stepLocation.line - 1,
+                    stepLocation.col - 1
+                )
+            });
         }
     }
 }
