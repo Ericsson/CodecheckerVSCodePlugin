000edeb56ff990d0db389a9b5f721a708d4cad89
Discookie <matekos17@fazekas.hu>
2021-09-27 02:44:54 +0200

    Add CodeChecker executor
    * Runs using ``.codechecker/compile_cmd.json``
    * Runs on save, with setting to toggle on/off
    * Has the ability to cancel an operation

---
+++ src/backend/executor/process.ts
@@ -0,0 +1,137 @@
+import * as child_process from 'child_process';
+import * as path from 'path';
+import { Event, EventEmitter, ExtensionContext, Uri, workspace } from 'vscode';
+
+export enum ProcessStatus {
+    notRunning,
+    running,
+    killed,
+    finished,
+    errored,
+}
+
+export class ExecutorProcess {
+    activeProcess?: child_process.ChildProcess;
+
+    private _processStdout: EventEmitter<string> = new EventEmitter();
+    public get processStdout(): Event<string> {
+        return this._processStdout.event;
+    }
+
+    private _processStderr: EventEmitter<string> = new EventEmitter();
+    public get processStderr(): Event<string> {
+        return this._processStderr.event;
+    }
+
+    private _processStatus: ProcessStatus = ProcessStatus.notRunning;
+    public get processStatus(): ProcessStatus {
+        return this._processStatus;
+    }
+
+    private _processStatusChange: EventEmitter<ProcessStatus> = new EventEmitter();
+    public get processStatusChange(): Event<ProcessStatus> {
+        return this._processStatusChange.event;
+    }
+
+    constructor(_ctx: ExtensionContext) {
+    }
+
+    private updateStatus(status: ProcessStatus) {
+        switch (status) {
+        case ProcessStatus.running:
+            if (this._processStatus !== ProcessStatus.running) {
+                this._processStatus = ProcessStatus.running;
+                this._processStatusChange.fire(ProcessStatus.running);
+            }
+            break;
+        default:
+            if (this._processStatus === ProcessStatus.running) {
+                this.activeProcess = undefined;
+                this._processStatus = ProcessStatus.notRunning;
+                this._processStatusChange.fire(status);
+            }
+            break;
+        }
+    }
+
+    /**
+     * If the arguments are empty, the entire project is analyzed
+     */
+    public startProcess(...files: Uri[]) {
+        if (this.activeProcess !== undefined) {
+            return;
+        }
+
+        if (!workspace.workspaceFolders?.length) {
+            // Silently ignore the call - user will be warned via other modules
+            return;
+        }
+
+        const workspaceFolder = workspace.workspaceFolders[0].uri.fsPath;
+
+        const getConfigAndReplaceVariables = (category: string, name: string): string | undefined => {
+            const configValue = workspace.getConfiguration(category).get<string>(name);
+            return configValue
+                ?.replace(/\${workspaceRoot}/g, workspaceFolder)
+                .replace(/\${workspaceFolder}/g, workspaceFolder)
+                .replace(/\${cwd}/g, process.cwd())
+                .replace(/\${env\.([^}]+)}/g, (sub: string, envName: string) => process.env[envName] ?? '');
+        };
+
+        const ccPath = getConfigAndReplaceVariables('codechecker.executor', 'executablePath')
+            ?? 'CodeChecker';
+        const ccFolder = getConfigAndReplaceVariables('codechecker.backend', 'outputFolder')
+            ?? path.join(workspaceFolder, '.codechecker');
+        const ccCompileCmd = path.join(ccFolder, 'compile_cmd.json');
+        const ccArguments = getConfigAndReplaceVariables('codechecker.executor', 'arguments') ?? '';
+        const ccThreads = workspace.getConfiguration('codechecker.executor').get<string>('threadCount');
+
+        const filePaths = files.length
+            ? `--file ${files.map((uri) => `"${uri.fsPath}"`).join(' ')}`
+            : '';
+
+        const commandLine = [
+            `${ccPath} analyze`,
+            `"${ccCompileCmd}"`,
+            `--output "${ccFolder}"`,
+            `${ccThreads ? '-j ' + ccThreads : ''}`,
+            `${ccArguments}`,
+            `${filePaths}`,
+        ].join(' ');
+
+        this._processStdout.fire(`> ${commandLine}\n`);
+        this.activeProcess = child_process.spawn(commandLine, { shell: true });
+        this.activeProcess.stdout!.on('data', (stdout: Buffer) => {
+            const decoded = stdout.toString();
+            this._processStdout.fire(decoded);
+        });
+
+        this.activeProcess.stderr!.on('data', (stderr) => {
+            const decoded = stderr.toString();
+            this._processStderr.fire(decoded);
+        });
+
+        this.activeProcess.on('error', (err) => {
+            this._processStdout.fire(`>>> Process errored: ${err.message}\n`);
+            this.updateStatus(ProcessStatus.errored);
+        });
+        this.activeProcess.on('exit', () => {
+            // FIXME: Errored process checking
+            this._processStdout.fire('>>> Process exited\n');
+            this.updateStatus(ProcessStatus.finished);
+        });
+
+        this.updateStatus(ProcessStatus.running);
+    }
+
+    public killProcess() {
+        if (this.activeProcess === undefined) {
+            return;
+        }
+
+        this.activeProcess.kill('SIGINT');
+        this._processStdout.fire('>>> Process killed\n');
+
+        this.updateStatus(ProcessStatus.killed);
+    }
+}
---
+++ src/backend/executor/triggers.ts
@@ -0,0 +1,242 @@
+import {
+    CancellationToken,
+    CustomExecution,
+    Disposable,
+    Event,
+    EventEmitter,
+    ExtensionContext,
+    ProviderResult,
+    Pseudoterminal,
+    Task,
+    TaskDefinition,
+    TaskProvider,
+    TerminalDimensions,
+    Uri,
+    commands,
+    tasks,
+    window,
+    workspace
+} from 'vscode';
+import { ExtensionApi } from '../api';
+import { ProcessStatus } from './process';
+
+enum ExecutorTaskType {
+    project = 'project',
+    currentFile = 'currentFile',
+    selectedFiles = 'selectedFiles'
+}
+
+class ExecutorTaskTerminal implements Pseudoterminal {
+    private _onDidWrite = new EventEmitter<string>();
+    onDidWrite: Event<string> = this._onDidWrite.event;
+
+    private _onDidClose = new EventEmitter<void>();
+    onDidClose?: Event<void> = this._onDidClose.event;
+
+    open(_initialDimensions?: TerminalDimensions): void {
+        this._onDidWrite.fire('Piping the console log is work-in-progress');
+    }
+
+    close(): void {
+        throw new Error('Method not implemented.');
+    }
+}
+
+class ExecutorTaskProvider implements TaskProvider<Task> {
+    taskID = 'CodeChecker';
+
+    constructor(private manager: ExecutorManager) {}
+
+    provideTasks(token: CancellationToken): ProviderResult<Task[]> {
+        if (workspace.workspaceFolders === undefined) {
+            return [];
+        }
+
+        const tasks = [
+            this.resolveTask(new Task(
+                { type: this.taskID, taskType: ExecutorTaskType.project as string },
+                workspace.workspaceFolders[0],
+                'Analyze project',
+                this.taskID
+            ), token)!,
+            this.resolveTask(new Task(
+                { type: this.taskID, taskType: ExecutorTaskType.currentFile as string },
+                workspace.workspaceFolders[0],
+                'Analyze current file',
+                this.taskID
+            ), token)!,
+            this.resolveTask(new Task(
+                { type: this.taskID, taskType: ExecutorTaskType.selectedFiles as string },
+                workspace.workspaceFolders[0],
+                'Analyze selected files...',
+                this.taskID
+            ), token)!,
+        ].filter(x => x !== undefined);
+
+        return tasks;
+    }
+
+    resolveTask(task: Task, _token: CancellationToken): Task | undefined {
+        const taskType: ExecutorTaskType = task.definition.taskType;
+
+        let callback: (() => void) | undefined;
+
+        switch (taskType) {
+        case ExecutorTaskType.project:
+            callback = () => this.manager.analyzeProject();
+            break;
+        case ExecutorTaskType.currentFile:
+            callback = () => this.manager.analyzeCurrentFile();
+            break;
+        case ExecutorTaskType.selectedFiles:
+            callback = () => this.manager.selectAndAnalyzeFile(task.definition.selectedFiles);
+            break;
+        default:
+            break;
+        }
+
+        if (callback === undefined) {
+            return undefined;
+        }
+
+        const executionBody = async (_definition: TaskDefinition) => {
+            const terminal = new ExecutorTaskTerminal();
+            callback!();
+            return terminal;
+        };
+
+        return new Task(
+            task.definition,
+            task.scope ?? workspace.workspaceFolders![0],
+            task.name,
+            task.source,
+            new CustomExecution(executionBody)
+        );
+    }
+}
+
+export class ExecutorManager implements Disposable {
+    private taskProvider: ExecutorTaskProvider;
+
+    private fileQueue: Uri[] = [];
+    private activeEvent?: Disposable;
+
+    constructor(ctx: ExtensionContext) {
+        workspace.onDidSaveTextDocument(this.analyzeOnSave, this, ctx.subscriptions);
+
+        ctx.subscriptions.push(
+            commands.registerCommand('codechecker.executor.analyzeCurrentFile', this.analyzeCurrentFile, this)
+        );
+        ctx.subscriptions.push(
+            commands.registerCommand('codechecker.executor.analyzeSelectedFiles', this.selectAndAnalyzeFile, this)
+        );
+        ctx.subscriptions.push(
+            commands.registerCommand('codechecker.executor.analyzeProject', this.analyzeProject, this)
+        );
+        ctx.subscriptions.push(commands.registerCommand('codechecker.executor.stopAnalysis', this.stopAnalysis, this));
+
+        this.taskProvider = new ExecutorTaskProvider(this);
+        ctx.subscriptions.push(tasks.registerTaskProvider(this.taskProvider.taskID, this.taskProvider));
+    }
+
+    dispose() {
+        if (this.activeEvent !== undefined) {
+            this.activeEvent.dispose();
+        }
+    }
+
+    analyzeOnSave() {
+        const canAnalyzeOnSave = workspace.getConfiguration('codechecker.executor').get<boolean>('runOnSave');
+
+        if (!canAnalyzeOnSave) {
+            return;
+        }
+
+        this.analyzeCurrentFile();
+    }
+
+    async selectAndAnalyzeFile(...files: string[]) {
+        if (files.length > 0) {
+            for (const file of files) {
+                this.analyzeFile(file);
+            }
+
+            return;
+        }
+
+        const selectedFiles = await window.showOpenDialog({ canSelectFiles: true, canSelectMany: true });
+
+        if (selectedFiles !== undefined) {
+            for (const file of selectedFiles) {
+                this.analyzeFile(file.fsPath);
+            }
+        }
+    }
+
+    analyzeCurrentFile() {
+        const currentFile = window.activeTextEditor?.document.uri.fsPath;
+
+        if (currentFile !== undefined) {
+            this.analyzeFile(currentFile);
+        }
+    }
+
+    analyzeFile(file: string) {
+        const uri = Uri.file(file);
+        this.fileQueue = this.fileQueue.filter((f) => f.fsPath !== uri.fsPath);
+        this.fileQueue.push(uri);
+
+        if (ExtensionApi.executorProcess.processStatus === ProcessStatus.running) {
+            // Queue up the process, and add the callback if it does not exist
+            if (this.activeEvent === undefined) {
+                this.activeEvent = ExtensionApi.executorProcess.processStatusChange(
+                    this.executeQueue,
+                    this
+                );
+            }
+            return;
+        }
+
+        this.executeQueue();
+    }
+
+    analyzeProject() {
+        // Kill the process, since the entire project is getting analyzed anyways
+        this.stopAnalysis();
+
+        ExtensionApi.executorProcess.startProcess();
+    }
+
+    stopAnalysis() {
+        this.fileQueue = [];
+
+        if (ExtensionApi.executorProcess.processStatus === ProcessStatus.running) {
+            ExtensionApi.executorProcess.killProcess();
+        }
+    }
+
+    private executeQueue(_status?: ProcessStatus) {
+        // Purge the event if there is nothing to execute
+        if (this.fileQueue.length === 0) {
+            this.activeEvent?.dispose();
+            this.activeEvent = undefined;
+            return;
+        }
+
+        if (ExtensionApi.executorProcess.processStatus === ProcessStatus.running) {
+            return;
+        }
+
+        // Only start the latest, and purge the queue
+        const latestFile = this.fileQueue.pop();
+        this.fileQueue = [];
+
+        if (latestFile) {
+            ExtensionApi.executorProcess.startProcess(latestFile);
+        }
+
+        // And then finally, purge the event
+        this.activeEvent?.dispose();
+        this.activeEvent = undefined;
+    }
+}
