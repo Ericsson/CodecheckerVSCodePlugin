65f74fb7d6507a9711b7f81cd21e92749c28b924
Discookie <matekos17@fazekas.hu>
2022-03-11 10:10:58 +0100

    Export extension members for testing

--- src/extension.ts
+++ src/extension.ts
@@ -3,13 +3,25 @@ import { ExtensionApi } from './backend/api';
 import { Editor } from './editor';
 import { SidebarContainer } from './sidebar';
 
-export function activate(context: vscode.ExtensionContext) {
+export interface CodeCheckerExtension {
+    extensionApi: typeof ExtensionApi,
+    sidebarContainer: typeof SidebarContainer,
+    editor: typeof Editor
+}
+
+export function activate(context: vscode.ExtensionContext): CodeCheckerExtension {
     // Backend must be initialized before the frontend
     ExtensionApi.init(context);
     Editor.init(context);
     SidebarContainer.init(context);
 
     console.log('Extension "codechecker" activated');
+
+    return {
+        extensionApi: ExtensionApi,
+        sidebarContainer: SidebarContainer,
+        editor: Editor
+    };
 }
 
 export function deactivate() {}
